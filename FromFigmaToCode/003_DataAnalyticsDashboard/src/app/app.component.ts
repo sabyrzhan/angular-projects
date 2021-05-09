55a4518683a22d65e1bac22078875098c70bb4f4
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-09 19:50:15 +0300

    Complete mobile version with author's version. Use angular animations to apply CSS3 animations.

--- FromFigmaToCode/003_DataAnalyticsDashboard/src/app/app.component.ts
+++ FromFigmaToCode/003_DataAnalyticsDashboard/src/app/app.component.ts
@@ -1,21 +1,35 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        transform: 'translateX(0)'
+      })),
+      state('closed', style({
+        transform: 'translateX(-100%)'
+      })),
+      transition('open => closed', [
+        animate('.3s')
+      ]),
+      transition('closed => open', [
+        animate('.3s')
+      ])
+      ]
+    )
+  ]
 })
 export class AppComponent {
   title = 'DataAnalyticsDashboard';
   @ViewChild('menu')
   menu?: ElementRef;
+  isOpen = false;
 
-  handleClickMenu(): void {
-    const div = this.menu!.nativeElement;
-    if (div.style.display === 'block') {
-      div.style.display = 'none';
-    } else {
-      div.style.display = 'block';
-    }
+  toggle(): void {
+    this.isOpen = !this.isOpen;
   }
 }
