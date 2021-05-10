896996328cab835bc9138cec3d0bbc3ff541ed08
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-11 02:55:44 +0300

    - Complete mobile version - use angular-fontawesome component for favorites - use angular animation for menu toggling

--- FromFigmaToCode/004_CarSalesSite/src/app/app.component.ts
+++ FromFigmaToCode/004_CarSalesSite/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
+import {IconPrefix, IconName} from '@fortawesome/fontawesome-svg-core';
+
+import {
+  transition,
+  animate, trigger, state, style
+} from '@angular/animations';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        clipPath: 'circle(100% at 50% 50%)'
+      })),
+      state('closed', style({
+        clipPath: 'circle(0% at 100% 0%)'
+      })),
+      transition('open => closed', [
+        animate('.3s ease-in-out')
+      ]),
+      transition('closed => open', [
+        animate('.3s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'CarSalesSite';
+  isOpen = false;
+
+  @ViewChild('filter')
+  containerDiv?: ElementRef;
+
+  constructor() {
+    document.addEventListener('mouseup', this.toggleMenu.bind(this));
+  }
+
+  toggleMenu(e?: any): void {
+    if (!this.containerDiv!.nativeElement.contains(e.target)) {
+      this.isOpen = false;
+    }
+  }
+
+  toggleHeart(iconComponent: FaIconComponent): void {
+    const icon = iconComponent.icon as [IconPrefix, IconName];
+    if (icon[0] === 'far') {
+      icon[0] = 'fas';
+    } else {
+      icon[0] = 'far';
+    }
+    iconComponent.render();
+  }
 }
--- FromFigmaToCode/004_CarSalesSite/src/app/app.module.ts
+++ FromFigmaToCode/004_CarSalesSite/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+}
