0ef8289cd434e88bae290dbb2149d4c7bd13e324
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-13 00:32:22 +0300

    Add styles to header and drone animation using angular animation.

--- FromFigmaToCode/005_DroneSite/src/app/app.component.ts
+++ FromFigmaToCode/005_DroneSite/src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animateBlur', [
+      state('down', style({
+        opacity: '.3'
+      })),
+      state('up', style({
+        opacity: '0'
+      })),
+      transition('down => up', [
+        animate('2s')
+      ]),
+      transition('up => down', [
+        animate('2s')
+      ])
+    ]),
+    trigger('animateDrone', [
+      state('down', style({
+        transform: 'translateY(0)'
+      })),
+      state('up', style({
+        transform: 'translateY(-30px)'
+      })),
+      transition('down => up', [
+        animate('2s')
+      ]),
+      transition('up => down', [
+        animate('2s')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'DroneSite';
+  blurState = 'down';
+  droneState = 'down';
+
+  onEnd(objectName: string): void {
+    const fn = (stateName: string) => {
+      if (stateName === 'down') {
+        return 'up';
+      } else {
+        return 'down';
+      }
+    };
+
+    if (objectName === 'blur') {
+      this.blurState = fn(this.blurState);
+    } else {
+      this.droneState = fn(this.droneState);
+    }
+  }
 }
