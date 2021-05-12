3eea2bdb3cb1d74acc9de1469db3e3e414de4187
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-13 01:27:08 +0300

    Add quote component

---
+++ FromFigmaToCode/005_DroneSite/src/app/quote-section/quote-section.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'quote-section',
+  templateUrl: './quote-section.component.html',
+  styleUrls: ['./quote-section.component.css']
+})
+export class QuoteSectionComponent {
+
+}
