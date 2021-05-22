958cfeb3d0059d92d8516f4d33a5c67cbf43068e
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-22 15:09:49 +0300

    Add page2, page3 and footer,header components

--- AngularForJavaDevelopers/001_SimpleAngularApp/src/app/app.module.ts
+++ AngularForJavaDevelopers/001_SimpleAngularApp/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
