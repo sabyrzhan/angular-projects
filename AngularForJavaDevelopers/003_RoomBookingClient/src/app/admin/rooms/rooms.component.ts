92990089ce294d04f0fe786c5cfeae4c5c58e528
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-05-30 23:07:47 +0300

    add room details and show rooms in rooms.component.html

--- AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/rooms.component.ts
+++ AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../../data.service';
 import {Room} from '../../model/Room';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
@@ -9,11 +10,23 @@ import {Room} from '../../model/Room';
 })
 export class RoomsComponent implements OnInit {
   rooms: Array<Room> = new Array<Room>();
+  selectedRoom?: Room;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.rooms = this.dataService.rooms;
+    this.route.queryParams.subscribe(param => {
+      if (param.id) {
+        this.selectRoom(+param.id);
+      }
+    });
   }
 
+  selectRoom(id: number): void {
+    this.selectedRoom = this.rooms.find(r => r.id === id);
+    this.router.navigate(['admin/rooms'], {queryParams: {id}});
+  }
 }
