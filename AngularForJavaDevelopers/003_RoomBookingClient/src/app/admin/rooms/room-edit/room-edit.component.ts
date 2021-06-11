1b3dded0f51c8cbfcddecafdb550b41098e7005e
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-06-12 00:27:15 +0300

    dynamically adding form control in code.

--- AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
+++ AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Room} from '../../../model/Room';
+import {Layout, LayoutCapacity, Room} from '../../../model/Room';
 import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
@@ -11,6 +11,9 @@ export class RoomEditComponent implements OnInit {
   @Input()
   room?: Room;
 
+  layouts?: [string, Layout][];
+  layout = Layout;
+
   roomForm = new FormGroup({
     roomName: new FormControl('roomName'),
     location: new FormControl('location')
@@ -19,16 +22,29 @@ export class RoomEditComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.layouts = Object.entries(Layout);
     this.roomForm.patchValue({
       roomName: this.room?.name,
       location: this.room?.location
     });
+    for (const layout of this.layouts) {
+      this.roomForm.addControl(`layout${layout[0]}`, new FormControl(`layout${layout[0]}`));
+    }
   }
 
   onSubmit(): void {
     if (this.room) {
       this.room.name = this.roomForm.controls.roomName.value;
       this.room.location = this.roomForm.controls.location.value;
+      if (this.layouts) {
+        this.room.capacities = new Array<LayoutCapacity>();
+        for (const layout of this.layouts) {
+          const capacity = new LayoutCapacity();
+          capacity.layout = layout[1];
+          capacity.capacity = this.roomForm.controls[`layout${layout[0]}`].value;
+          this.room.capacities.push(capacity);
+        }
+      }
     }
 
     console.log(this.room);
