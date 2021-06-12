45ce4295744bb7d89de9a4852816431bbbd46042
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-06-12 21:20:23 +0300

    Use FormBuilder to add controls

--- AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
+++ AngularForJavaDevelopers/003_RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Layout, LayoutCapacity, Room} from '../../../model/Room';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-room-edit',
@@ -14,34 +14,34 @@ export class RoomEditComponent implements OnInit {
   layouts?: [string, Layout][];
   layout = Layout;
 
-  roomForm = new FormGroup({
-    roomName: new FormControl('roomName'),
-    location: new FormControl('location')
-  });
+  roomForm?: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.layouts = Object.entries(Layout);
-    this.roomForm.patchValue({
+    this.roomForm = this.formBuilder.group({
       roomName: this.room?.name,
       location: this.room?.location
     });
+
     for (const layout of this.layouts) {
-      this.roomForm.addControl(`layout${layout[0]}`, new FormControl(`layout${layout[0]}`));
+      const layoutCapacity = this.room?.capacities.find(cap => cap.layout === layout[1]);
+      const initialCapacity = layoutCapacity ? layoutCapacity.capacity : 0;
+      this.roomForm.addControl(`layout${layout[0]}`, this.formBuilder.control(initialCapacity));
     }
   }
 
   onSubmit(): void {
     if (this.room) {
-      this.room.name = this.roomForm.controls.roomName.value;
-      this.room.location = this.roomForm.controls.location.value;
+      this.room.name = this.roomForm?.controls.roomName.value;
+      this.room.location = this.roomForm?.controls.location.value;
       if (this.layouts) {
         this.room.capacities = new Array<LayoutCapacity>();
         for (const layout of this.layouts) {
           const capacity = new LayoutCapacity();
           capacity.layout = layout[1];
-          capacity.capacity = this.roomForm.controls[`layout${layout[0]}`].value;
+          capacity.capacity = this.roomForm?.controls[`layout${layout[0]}`].value;
           this.room.capacities.push(capacity);
         }
       }
