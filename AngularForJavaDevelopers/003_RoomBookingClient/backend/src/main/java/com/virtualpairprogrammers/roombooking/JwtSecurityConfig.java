49646a613cd1d7ee39658e155d8a567d31c2b914
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-06-30 01:14:47 +0300

    Seperate basic and jwt secruity config.

--- AngularForJavaDevelopers/003_RoomBookingClient/backend/src/main/java/com/virtualpairprogrammers/roombooking/JwtSecurityConfig.java
+++ AngularForJavaDevelopers/003_RoomBookingClient/backend/src/main/java/com/virtualpairprogrammers/roombooking/JwtSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.virtualpairprogrammers.roombooking;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
